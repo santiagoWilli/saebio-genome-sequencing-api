62b0f2013db5081e1503027709e44682501c7cdf
santiagoWilli <santiago30mw@gmail.com>
2021-01-15 19:01:54 +0000

    Adopted new test naming convention and implemented test given_noFiles_when_postToSequencies_then_statusCode400 and modified previous createASequencyShouldReturn201 to given_aPairOfFiles_when_postToSequencies_then_statusCode201

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,8 +1,19 @@
+import javax.servlet.MultipartConfigElement;
+import javax.servlet.http.Part;
+
 import static spark.Spark.*;
 
 public class Application {
     public static void main(String[] args) {
         post("/sequencies", (request, response) -> {
+            request.attribute("org.eclipse.jetty.multipartConfig", new MultipartConfigElement("/temp"));
+            try {
+                Part filePart = request.raw().getPart("pair1");
+            } catch (Exception e) {
+                response.status(400);
+                return "";
+            }
+
             response.status(201);
             return "";
         });
--- src/test/java/Application_.java
+++ src/test/java/Application_.java
@@ -1,6 +1,9 @@
+import org.bson.Document;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.*;
+
+import java.io.File;
 
 public class Application_ {
     static {
@@ -8,8 +11,19 @@ public class Application_ {
     }
 
     @Test
-    public void createASequencyShouldReturn201() {
+    public void given_noFiles_when_postToSequencies_then_statusCode400() {
+        when().
+                post("/sequencies").
+        then().
+                statusCode(400);
+    }
+
+    @Test
+    public void given_aPairOfFiles_when_postToSequencies_then_statusCode201() {
         given().
+                multiPart("pair1", new File("test/sequencies/Kpneu1_191120_R1.fastq.gz")).
+                multiPart("pair2", new File("test/sequencies/Kpneu1_191120_R2.fastq.gz")).
+        when().
                 post("/sequencies").
         then().
                 statusCode(201);
