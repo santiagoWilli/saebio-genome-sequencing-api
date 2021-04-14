989d5befe1e3fff46c507ad49ed45aec536bd4f3
santiagoWilli <santiago30mw@gmail.com>
2021-04-14 19:28:02 +0100

    Temp files removal also when exception encountered in handle() method

--- src/main/java/handlers/AbstractHandler.java
+++ src/main/java/handlers/AbstractHandler.java
@@ -36,38 +36,48 @@ public abstract class AbstractHandler<V extends Validable> implements RequestHan
     protected abstract Answer processRequest(V payload, Map<String, String> requestParams);
 
     @Override
-    public Object handle(Request request, Response response) throws Exception {
-        V payload = null;
+    public Object handle(Request request, Response response) throws IOException {
         String uuid = null;
-        if (payloadClass != EmptyPayload.class) {
-            request.attribute("org.eclipse.jetty.multipartConfig", new MultipartConfigElement("/temp"));
-            Map<String, String> fields = new HashMap<>();
-            Map<String, File> files = new HashMap<>();
+        try {
+            V payload = null;
+            if (payloadClass != EmptyPayload.class) {
+                request.attribute("org.eclipse.jetty.multipartConfig", new MultipartConfigElement("/temp"));
+                Map<String, String> fields = new HashMap<>();
+                Map<String, File> files = new HashMap<>();
 
-            uuid = UUID.randomUUID().toString();
-            for (Part part : request.raw().getParts()) {
-                if (part.getName().matches("^file[1-2]$")) {
-                    File file = new File("temp/" + uuid + "/" + part.getName());
-                    FileUtils.copyInputStreamToFile(part.getInputStream(), file);
-                    files.put(part.getSubmittedFileName(), file);
-                } else {
-                    String value = new BufferedReader(
-                            new InputStreamReader(part.getInputStream(), StandardCharsets.UTF_8))
+                uuid = UUID.randomUUID().toString();
+                for (Part part : request.raw().getParts()) {
+                    if (part.getName().matches("^file[1-2]$")) {
+                        File file = new File("temp/" + uuid + "/" + part.getName());
+                        FileUtils.copyInputStreamToFile(part.getInputStream(), file);
+                        files.put(part.getSubmittedFileName(), file);
+                    } else {
+                        String value = new BufferedReader(
+                                new InputStreamReader(part.getInputStream(), StandardCharsets.UTF_8))
                                 .lines()
                                 .collect(Collectors.joining("\n"));
-                    fields.put(part.getName(), value);
+                        fields.put(part.getName(), value);
+                    }
+                    part.delete();
                 }
-                part.delete();
+                payload = payloadClass.getConstructor(Map.class, Map.class).newInstance(fields, files);
             }
-            payload = payloadClass.getConstructor(Map.class, Map.class).newInstance(fields, files);
+
+            Answer answer = process(payload, request.params());
+            deleteTempFiles(uuid);
+
+            response.status(answer.getCode());
+            response.type("application/json");
+            return answer.getBody();
+        } catch (Exception e) {
+            deleteTempFiles(uuid);
+            response.status(500);
+            response.type("application/json");
+            return Answer.serverError(e.getMessage());
         }
+    }
 
-        Answer answer = process(payload, request.params());
-
+    private void deleteTempFiles(String uuid) throws IOException {
         if (payloadClass != EmptyPayload.class) FileUtils.deleteDirectory(new File("temp/" + uuid));
-
-        response.status(answer.getCode());
-        response.type("application/json");
-        return answer.getBody();
     }
 }
