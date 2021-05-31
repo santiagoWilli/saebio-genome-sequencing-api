944bef6aa4eb1cdeab29c664167e39f52641900d
santiagoWilli <santiago30mw@gmail.com>
2021-05-31 16:43:40 +0100

    UserAuthentication getPassword_should_returnThePassword & getUsername_should_returnTheUsername

---
+++ src/main/java/payloads/UserAuthentication.java
@@ -0,0 +1,22 @@
+package payloads;
+
+import java.util.Map;
+
+public class UserAuthentication extends RequestParameters implements Validable {
+    public UserAuthentication(Map<String, String[]> parameters) {
+        super(parameters);
+    }
+
+    @Override
+    public boolean isValid() {
+        return false;
+    }
+
+    public String getUsername() {
+        return parameters.get("username")[0];
+    }
+
+    public String getPassword() {
+        return parameters.get("password")[0];
+    }
+}
---
+++ src/test/java/unit/payloads/UserAuthentication_.java
@@ -0,0 +1,33 @@
+package unit.payloads;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import payloads.UserAuthentication;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class UserAuthentication_ {
+    private Map<String, String[]> parameters;
+    private UserAuthentication authentication;
+
+    @Test
+    public void getUsername_should_returnTheUsername() {
+        parameters.put("username", new String[]{"name"});
+        assertThat(authentication.getUsername()).isEqualTo("name");
+    }
+
+    @Test
+    public void getPassword_should_returnThePassword() {
+        parameters.put("password", new String[]{"anyPass"});
+        assertThat(authentication.getPassword()).isEqualTo("anyPass");
+    }
+
+    @BeforeEach
+    public void setUp() {
+        parameters = new HashMap<>();
+        authentication = new UserAuthentication(parameters);
+    }
+}
