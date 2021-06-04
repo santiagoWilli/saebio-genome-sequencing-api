e7b3a5ff5c1ed0a42698ad9ef9051331b9216377
santiagoWilli <santiago30mw@gmail.com>
2021-06-04 12:16:14 +0100

    CORS added

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -40,14 +40,10 @@ public class Application {
         Database.setDatabaseName(options.database);
         Database.setHost(options.dbHost);
 
+        enableCORS();
+
         path("/api", () -> {
-            before("/*", (request, response) -> {
-                if (request.uri().endsWith("/login") || request.uri().endsWith("/alive")) return;
-                String jwt = request.headers("Authorization");
-                if (jwt == null || jwt.isEmpty()) halt(401, "Authorization required");
-                jwt = jwt.replace("Bearer ", "");
-                if (!JWT.verify(jwt)) halt(401, "Authorization not valid");
-            });
+            handleAuthorization();
 
             get("/alive", (request, response) -> "I am alive!");
 
@@ -84,6 +80,35 @@ public class Application {
         });
 
         exception(Exception.class, (exception, request, response) -> System.out.println(" -ERROR: " + exception.getMessage()));
+    }
+
+    private static void enableCORS() {
+        options("/*",
+                (request, response) -> {
+                    String accessControlRequestHeaders = request.headers("Access-Control-Request-Headers");
+                    if (accessControlRequestHeaders != null) {
+                        response.header("Access-Control-Allow-Headers", accessControlRequestHeaders);
+                    }
+
+                    String accessControlRequestMethod = request.headers("Access-Control-Request-Method");
+                    if (accessControlRequestMethod != null) {
+                        response.header("Access-Control-Allow-Methods", accessControlRequestMethod);
+                    }
+
+                    return "OK";
+                });
+
+        before((request, response) -> response.header("Access-Control-Allow-Origin", "*"));
+    }
+
+    private static void handleAuthorization() {
+        before("/*", (request, response) -> {
+            if (request.uri().endsWith("/login") || request.uri().endsWith("/alive")) return;
+            String jwt = request.headers("Authorization");
+            if (jwt == null || jwt.isEmpty()) halt(401, "Authorization required");
+            jwt = jwt.replace("Bearer ", "");
+            if (!JWT.verify(jwt)) halt(401, "Authorization not valid");
+        });
     }
 
     private static void printHelp() {
