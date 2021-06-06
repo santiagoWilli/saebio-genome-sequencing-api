217ff455cb03c084065f0c94c7a8f5afa7549363
santiagoWilli <santiago30mw@gmail.com>
2021-06-06 22:02:49 +0100

    CORS fixed

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -98,11 +98,15 @@ public class Application {
                     return "OK";
                 });
 
-        before((request, response) -> response.header("Access-Control-Allow-Origin", "*"));
+        before((request, response) -> {
+            response.header("Access-Control-Allow-Origin", "*");
+            response.header("Access-Control-Request-Method", "GET,PATCH,POST,DELETE,OPTIONS");
+            response.header("Access-Control-Allow-Headers", "Content-Type,Authorization,X-Requested-With,Content-Length,Accept,Origin");
+        });
     }
 
     private static void handleAuthorization() {
-        before("/*", (request, response) -> {
+        before((request, response) -> {
             if (request.uri().endsWith("/login") || request.uri().endsWith("/alive")) return;
             String jwt = request.headers("Authorization");
             if (jwt == null || jwt.isEmpty()) halt(401, "Authorization required");
